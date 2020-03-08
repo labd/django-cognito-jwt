58e61201974ab564a4c21d297724fce125f7d3e4
Michael van Tellingen <michael@mvantellingen.nl>
2020-03-08 20:28:41 +0100

    Add GitHub Actions integration

--- setup.py
+++ setup.py
@@ -1,61 +1,58 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    'Django>=1.11',
-    'cryptography',
-    'djangorestframework',
-    'pyjwt',
-    'requests',
+    "Django>=1.11",
+    "cryptography",
+    "djangorestframework",
+    "pyjwt",
+    "requests",
 ]
 
-
 docs_require = [
-    'sphinx>=1.4.0',
+    "sphinx>=1.4.0",
 ]
 
 tests_require = [
-    'coverage==.4.4.2',
-    'pytest==3.3.2',
-    'pytest-cov==2.5.1',
-    'pytest-django==3.1.2',
-    'pytest-responses==0.3.0',
-
+    "coverage[toml]==5.0.3",
+    "pytest==5.3.5",
+    "pytest-django==3.8.0",
+    "pytest-cov==2.8.1",
+    "pytest-responses==0.4.0",
     # Linting
-    'isort==4.2.15',
-    'flake8==3.5.0',
-    'flake8-blind-except==0.1.1',
-    'flake8-imports==0.1.1',
-    'flake8-debugger==3.0.0',
+    "isort[pyproject]==4.3.21",
+    "flake8==3.7.9",
+    "flake8-imports==0.1.1",
+    "flake8-blind-except==0.1.1",
+    "flake8-debugger==3.1.0",
 ]
 
 setup(
-    name='django-cognito-jwt',
-    version='0.0.3',
+    name="django-cognito-jwt",
+    version="0.0.3",
     description="Django backends for AWS Cognito JWT",
-    long_description=open('README.rst', 'r').read(),
-    url='https://github.com/LabD/django-cognito-jwt',
+    long_description=open("README.rst", "r").read(),
+    url="https://github.com/LabD/django-cognito-jwt",
     author="Michael van Tellingen",
     author_email="m.vantellingen@labdigital.nl",
     install_requires=install_requires,
-    extras_require={
-        'docs': docs_require,
-        'test': tests_require,
-    },
+    extras_require={"docs": docs_require, "test": tests_require,},
     use_scm_version=True,
     entry_points={},
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     zip_safe=False,
 )
