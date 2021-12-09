def66f21814fa9975df06e395afa860a1590c16c
Michel Moreau <michmoreau.l@gmail.com>
2021-12-09 08:20:17 -0500

    Replace ugettext and smart_text with proper functions for Django 4 compatibility
    see https://github.com/labd/django-cognito-jwt/issues/39

--- src/django_cognito_jwt/backend.py
+++ src/django_cognito_jwt/backend.py
@@ -2,8 +2,8 @@ import logging
 
 from django.apps import apps as django_apps
 from django.conf import settings
-from django.utils.encoding import smart_text
-from django.utils.translation import ugettext as _
+from django.utils.encoding import force_str
+from django.utils.translation import gettext as _
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication, get_authorization_header
 
