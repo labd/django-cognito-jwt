9446f625cfd112b273547a5c2c7f78834622c032
Michel Moreau <michmoreau.l@gmail.com>
2021-12-10 11:36:21 -0500

    Replace ugettext and smart_text with proper functions for Django 4 compatibility (#40)
    * Replace ugettext and smart_text with proper functions for Django 4 compatibility
    
    see https://github.com/labd/django-cognito-jwt/issues/39
    
    * replace smart_text to force_str

--- src/django_cognito_jwt/backend.py
+++ src/django_cognito_jwt/backend.py
@@ -2,8 +2,8 @@ import logging
 
 from django.apps import apps as django_apps
 from django.conf import settings
-from django.utils.encoding import smart_text
-from django.utils.translation import ugettext as _
+from django.utils.encoding import force_str
+from django.utils.translation import gettext as _
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication, get_authorization_header
 
@@ -38,7 +38,7 @@ class JSONWebTokenAuthentication(BaseAuthentication):
 
     def get_jwt_token(self, request):
         auth = get_authorization_header(request).split()
-        if not auth or smart_text(auth[0].lower()) != "bearer":
+        if not auth or force_str(auth[0].lower()) != "bearer":
             return None
 
         if len(auth) == 1:
